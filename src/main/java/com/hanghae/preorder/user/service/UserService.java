768c6bbc41ba94c12afbad469c04e70f2221dcd9
cod2048 <cod2048@gmail.com>
2024-01-30 01:59:29 +0900

    feat: JWT 구현

--- src/main/java/com/hanghae/preorder/user/service/UserService.java
+++ src/main/java/com/hanghae/preorder/user/service/UserService.java
@@ -2,17 +2,22 @@ package com.hanghae.preorder.user.service;
 
 import com.hanghae.preorder.email.service.EmailService;
 import com.hanghae.preorder.redis.service.RedisService;
+import com.hanghae.preorder.security.jwt.JwtTokenProvider;
+import com.hanghae.preorder.security.jwt.TokenType;
+import com.hanghae.preorder.user.dto.request.LoginRequest;
 import com.hanghae.preorder.user.dto.request.UserRequest;
-import com.hanghae.preorder.user.dto.request.VerificationRequest;
+import com.hanghae.preorder.user.dto.response.LoginResponse;
 import com.hanghae.preorder.user.dto.response.UserResponse;
 import com.hanghae.preorder.user.entity.User;
 import com.hanghae.preorder.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.juli.VerbatimFormatter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -23,30 +28,44 @@ public class UserService {
     private final UserRepository userRepository;
     private final RedisService redisService;
     private final EmailService emailService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Value("${spring.mail.auth-code-expiration-millis}")
     private Long timeout;
 
+    @Transactional
     public UserResponse create(UserRequest userRequest) {
-        //1. userRequest로 entity 생성
-        User newUser = new User(
+
+        String emailCode = userRequest.getVerificationCode();
+        String userEmail = userRequest.getEmail();
+
+        boolean checkCode = redisService.compareValue(userEmail, emailCode);
+
+        if(checkCode){
+            String encodingPassword = bCryptPasswordEncoder.encode(userRequest.getPassword());
+            //1. userRequest로 entity 생성
+            User newUser = new User(
                 userRequest.getEmail(),
-                userRequest.getPassword(),
+                encodingPassword,
                 userRequest.getName(),
                 userRequest.getProfileImage(),
                 userRequest.getGreeting()
-        );
+            );
 
-        //2. 생성된 entity db에 저장
-        User createdUser = userRepository.save(newUser);
+            //2. 생성된 entity db에 저장
+            User createdUser = userRepository.save(newUser);
 
-        //3. 인증 메일 전송
-
-        //4. entity를 userResponse 변환해서 반환
-        return new UserResponse(
+        //3. entity를 userResponse 변환해서 반환
+            return new UserResponse(
                 createdUser.getId(),
                 createdUser.getName()
-        );
+            );
+        }
+        else{
+            throw new IllegalStateException("can't create user");
+        }
+
     }
 
     public void verifyEmail(String email){
@@ -66,7 +85,36 @@ public class UserService {
         return String.valueOf(code);
     }
 
-    public UserResponse update(Long id, UserRequest userRequest) {
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        //1. request의 email이랑 password랑 db의 email password 검사
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        Optional<User> loginUser = userRepository.findByEmail(email);
+        log.info("로그인 서비스 진입(email,password가져오고, 유저찾기)");
+        //2중 if문 쓰지말고 findByEmail뒤에 throw추가하기
+
+        if(loginUser.isPresent()) {
+            log.info("유저 찾기 성공");
+            User user = loginUser.get();
+            String userName = user.getName();
+            if (bCryptPasswordEncoder.matches(password, user.getPassword())){
+                log.info("인코딩 패스워드 검증 성공");
+                String accessToken = jwtTokenProvider.generate(email, userName, TokenType.ACCESS);
+                String refreshToken = jwtTokenProvider.generate(email, userName, TokenType.REFRESH);
+
+                return new LoginResponse(accessToken, refreshToken, email, userName);
+            }
+            else{
+                throw new IllegalStateException("비밀번호 불일치");
+            }
+        }
+        else{
+            throw new IllegalStateException("유저 정보 없음");
+        }
+    }
+
+    public UserResponse update(Long id, Long requestId, UserRequest userRequest) {
         //1. 받아온 id로 db에 user 존재 유무 확인
         User target = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("can't find user"));
