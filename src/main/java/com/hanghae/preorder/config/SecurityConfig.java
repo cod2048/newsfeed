aa1e1cf8531ee6d599f2b5f1b556467b7868fc44
cod2048 <cod2048@gmail.com>
2024-01-29 22:14:36 +0900

    feat: 패스워드 암호화 config파일 추가

---
+++ src/main/java/com/hanghae/preorder/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.hanghae.preorder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+//    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+//    public SecurityConfig(final JwtAuthenticationFilter jwtAuthenticationFilter, final CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+//        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/newsfeeds").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/mail").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/refreshToken").permitAll()
+                        .anyRequest().authenticated())
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling(handle -> handle.authenticationEntryPoint(customAuthenticationEntryPoint))
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
