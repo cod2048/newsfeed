41d12dc0c6db4c419b85227101a8f0ee26fcc7bf
cod2048 <cod2048@gmail.com>
2024-02-09 00:47:44 +0900

    refactor: follow 테이블 수정
    - msa로 분리를 위해 연결관계 해제
    - softdelete를 위해 status 추가
    - 팔로우 해제 메소드 추가
    - 팔로우 상태 변경 시간 업데이트 메소드 추가

--- module_activity/src/main/java/com/hanghae/module_activity/follow/entity/Follow.java
+++ module_activity/src/main/java/com/hanghae/module_activity/follow/entity/Follow.java
@@ -1,8 +1,10 @@
 package com.hanghae.module_activity.follow.entity;
 
-import com.hanghae.module_activity.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -11,18 +13,33 @@ public class Follow {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User follower;
+    @Column(name = "follower_id")
+    private Long followerId;
 
-    @ManyToOne
-    @JoinColumn(name = "following")
-    private User following;
+    @Column(name = "following_id")
+    private Long followingId;
+
+    @Column(name = "status")
+    private Boolean status = true;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     public Follow() { }
 
-    public Follow(User follower, User following){
-        this.follower = follower;
-        this.following = following;
+    public Follow(Long follower, Long following){
+        this.followerId = follower;
+        this.followingId = following;
+    }
+
+    // 팔로우 해제 메소드
+    public void deactivate() {
+        this.status = false;
+    }
+
+    // 팔로우 상태 변경 시 시간 업데이트
+    public void updateTimestamp() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
