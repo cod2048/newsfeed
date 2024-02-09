fcf7cd81076563cdc6ee369b36a68b4aa843513e
cod2048 <cod2048@gmail.com>
2024-02-09 20:51:14 +0900

    style: 코드 스타일 변경(엔터)

--- src/main/java/com/hanghae/preorder/user/entity/User.java
+++ src/main/java/com/hanghae/preorder/user/entity/User.java
@@ -10,16 +10,22 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+
     @Column(name = "password", nullable = false)
     private String password;
+
     @Column(name = "name", nullable = false)
     private String name;
+
     @Column(name = "profile_image", nullable = false)
     private String profileImage;
+
     @Column(name = "greeting", nullable = false)
     private String greeting;
+
     public User() {
     }
 
