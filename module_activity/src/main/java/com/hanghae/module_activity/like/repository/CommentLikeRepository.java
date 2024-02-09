73b91b5220030f399c20e14eb02e5d7c2cfd9cad
cod2048 <cod2048@gmail.com>
2024-02-09 17:12:52 +0900

    remove: 좋아요 기능 하나로 통합
    - 기존 포스트 좋아요와 댓글 좋아요를 좋아요 하나로 통합
    - entity에 type추가(포스트인지 댓글인지 확인)

--- module_activity/src/main/java/com/hanghae/module_activity/like/repository/CommentLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hanghae.module_activity.like.repository;
-
-import com.hanghae.module_activity.like.entity.CommentLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
-}
